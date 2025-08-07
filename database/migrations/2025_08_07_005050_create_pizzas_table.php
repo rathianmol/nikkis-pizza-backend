b684410874ba723e15d806c09f3a0248c102f06a
rathianmol <rathianmol25@gmail.com>
2025-08-06 20:55:31 -0400

    Adding pizza-table migration to the system.

---
+++ database/migrations/2025_08_07_005050_create_pizzas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image')->nullable(); // Store image path/URL
+            $table->text('description');
+            $table->decimal('price_small', 8, 2);
+            $table->decimal('price_medium', 8, 2);
+            $table->decimal('price_large', 8, 2);
+            $table->decimal('price_xlarge', 8, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};
