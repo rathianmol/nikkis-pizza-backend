895738fe271f6b605f787d1dc4f268315ad39025
rathianmol <rathianmol25@gmail.com>
2025-10-28 08:25:43 -0400

    Adding fillable attributes to the store-locations-details model.

--- app/Models/StoreLocation.php
+++ app/Models/StoreLocation.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StoreLocation extends Model
 {
-    //
+    use Hasfactory;
+    
+    protected $fillable = [
+        'name',
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+        'phone_number',
+        'email',
+        'monday_hours',
+        'tuesday_hours',
+        'wednesday_hours',
+        'thursday_hours',
+        'friday_hours',
+        'saturday_hours',
+        'sunday_hours',
+        'is_active',
+        'is_primary',
+    ];
 }
