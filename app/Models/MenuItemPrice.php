0251dc7044bd12ce641d819e63048db9b78de870
rathianmol <rathianmol25@gmail.com>
2025-11-06 03:30:34 -0500

    adding model logic for menu-item-price object.

--- app/Models/MenuItemPrice.php
+++ app/Models/MenuItemPrice.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItemPrice extends Model
 {
-    //
+        protected $fillable = [
+        'menu_item_id',
+        'size',
+        'price'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
+
+    public function menuItem()
+    {
+        return $this->belongsTo(MenuItem::class);
+    }
 }
